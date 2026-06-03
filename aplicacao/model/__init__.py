6a69bf4d985ec1d549b11ebb356233afff2b206c
Pedro Camacho Ucsal <pedro.camacho@ucsal.edu.br>
2026-06-03 19:44:25 -0300

    Delete aplicacao directory

--- aplicacao/model/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Usuario import Usuario
-from .Cidadao import Cidadao
-from .Empresa import Empresa
-from .Admin import Admin
-from .Localizacao import Localizacao, RegistroMassa
-from .Oferta import Oferta
-from .Voucher import Voucher
