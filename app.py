877f1f73bbba7c8ef1254d038b09f81debfbf086
YuanLucas-Ucsal <yuan.sousa@ucsal.edu.br>
2026-06-04 11:05:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -538,8 +538,9 @@ def listar_vouchers_empresa(empresa_id):
 def catch_all(path): 
     return render_template('index.html') 
 
+with app.app_context(): 
+    db.create_all() 
 
 if __name__ == '__main__': 
-    with app.app_context(): 
-        db.create_all() 
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
